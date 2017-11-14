a86c0cb41ccbb0d9bf243f2dd8cde77af94e96c8
Prasanna Gautam <prasannagautam@gmail.com>
2017-11-14 15:35:40 -0800

    use pflag to make it unixy

--- logrotate-sync.go
+++ logrotate-sync.go
@@ -1,46 +1,41 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"log"
+	"os"
 	"regexp"
-	"strings"
 
 	"github.com/prasincs/logrotate-sync/processing"
+	flag "github.com/spf13/pflag"
 )
-
-type stringSlice []string
-
-// Implement the Value interface
-func (s *stringSlice) String() string {
-	return strings.Join(*s, ",")
-}
-
-func (s *stringSlice) Value() []string {
-	return *s
-}
-
-func (s *stringSlice) Set(value string) error {
-	*s = append(*s, value)
-	return nil
-}
 
 func main() {
 	var (
-		dirs           = stringSlice{}
+		dirs           = []string{}
 		compressGzip   = false
 		uploadPath     = ""
+		statePath      = ""
 		uploadPattern  = "2006/01/02"
-		filterLogTypes = stringSlice{}
-		//hostname       = ""
+		filterLogTypes = []string{}
+		hostname       = ""
+		err            error
 	)
-	flag.Var(&dirs, "dir", "Dir(s) to read the log files from")
+	flag.StringSliceVar(&dirs, "dir", []string{"."}, "Dir(s) to read the log files from")
 	flag.BoolVar(&compressGzip, "compress-gzip", false, "Compress log files if not already gzipped")
-	flag.StringVar(&uploadPath, "uploadpath", "", "s3://<bucket>/<prefix>")
-	flag.StringVar(&uploadPattern, "upload.pattern", "2006/01/02", "Directory pattern in Go time format")
-	flag.Var(&filterLogTypes, "logtype", "Types of logs to send to S3, by default sends everything")
+	flag.StringVar(&uploadPath, "upload-path", "", "s3://<bucket>/<prefix>")
+	flag.StringVar(&statePath, "state-path", "/var/run/logrotatesync.json", "Where to store the json of state of files that are available")
+	flag.StringVar(&uploadPattern, "upload-time-pattern", "2006/01/02", "Directory pattern in Go time format")
+	flag.StringSliceVar(&filterLogTypes, "logtype", []string{"."}, "Types of logs to send to S3, by default sends everything")
+	flag.StringVar(&hostname, "hostname", "", "Hostname to use, takes the default from os.Hostname() by default")
 	flag.Parse()
+
+	if hostname == "" {
+		hostname, err = os.Hostname()
+		if err != nil {
+			log.Fatalf("Unable to read the hostname: %s", err)
+		}
+	}
 
 	logPattern := regexp.MustCompile(`(?P<log_type>\S+).log(-|.)(?P<year>\d{4})-(?P<month>\d{2})-(?P<day>\d{2})-(?P<extra>\d+).?(?P<compression>gz)?`)
 	for _, d := range dirs {
